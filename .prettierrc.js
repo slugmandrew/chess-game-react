997511b8915a52d1d0bac5a29bbacf5721ce40cf
Drew Spencer <slugmandrew@gmail.com>
2021-07-25 12:37:51 +0100

    Add animations

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,7 +3,7 @@ module.exports = {
   tabWidth: 2,
   semi: false,
   singleQuote: true,
-  printWidth: 200,
+  printWidth: 160,
   arrowParens: 'always',
   bracketSpacing: true,
   jsxBracketSameLine: true,
--- src/components/Draggable.tsx
+++ src/components/Draggable.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
 import { useDraggable } from '@dnd-kit/core'
 import { CSS } from '@dnd-kit/utilities'
+import { AnimatePresence, motion } from 'framer-motion'
 
 export const Draggable: FC<{ id: string }> = ({ id, children }) => {
   const { setNodeRef, listeners, attributes, transform, active, isDragging } = useDraggable({ id: id })
@@ -17,11 +18,20 @@ export const Draggable: FC<{ id: string }> = ({ id, children }) => {
         style={{
           transform: CSS.Translate.toString(transform),
           position: 'absolute',
-        }}
-        ref={setNodeRef}
-        {...listeners}
-        {...attributes}>
-        {children}
+        }}>
+        <AnimatePresence>
+          <motion.div
+            key={id}
+            animate={{ scale: [1, 1.4, 1], rotate: [0, 360] }}
+            initial={true}
+            transition={{ duration: 0.8, ease: 'easeInOut' }}
+            exit={{ translateX: 200, translateY: 500 }}
+            ref={setNodeRef}
+            {...listeners}
+            {...attributes}>
+            {children}
+          </motion.div>
+        </AnimatePresence>
       </div>
     </>
   )
